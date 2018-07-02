eafe9c52206f4b6162d56d112d2e1a77029c38be
Eric Artzt <artzte@baddabigboom.com>
2018-07-02 10:50:20 -0700

    Add coordinator filtering test

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -1,23 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Students API', type: :request do
+
   before(:all) do
     @staff1 = create :user, privilege: 2
+    @staff2 = create :user, privilege: 2
     @student1 = create :user, coordinator: @staff1
-    @student2 = create :user, coordinator: @staff1
+    @student2 = create :user, coordinator: @staff2
+    @studentInactive = create :user, coordinator: @staff2, status: 2
   end
 
-  describe 'GET /students' do
-    before { get '/students' }
-
-    it 'returns students' do
+  describe 'GET /students ' do
+    it 'returns all active students by default' do
+      get '/students'
       expect(json).not_to be_empty
       expect(json.size).to eq(2)
     end
 
     it 'returns status code 200' do
+      get '/students'
       expect(response).to have_http_status(200)
     end
+
+    it 'returns students of a given coordinator' do
+      get "/students?coordinator_id=#{@staff1.id}"
+      expect(json).not_to be_empty
+      expect(json.size).to eq(1)
+    end
+
   end
 
 end
