aff0e0da1cb40906c6ac1728980aa90ea0572571
Eric Artzt <artzte@baddabigboom.com>
2019-03-06 08:54:00 -0500

    Clean up status serialization

--- app/serializers/status_serializer.rb
+++ app/serializers/status_serializer.rb
@@ -1,15 +1,16 @@
 class StatusSerializer < ApplicationSerializer
   set_type :status
 
-  attributes :month, :academic_status, :attendance_status, :created_at, :updated_at, :fte_hours, :met_fte_requirements, :held_periodic_checkins
+  attributes :month, :created_at, :updated_at, :fte_hours, :met_fte_requirements, :held_periodic_checkins
+
+  attribute :academic_status do |record|
+    Status::STATUS_NAMES[record.academic_status].downcase
+  end
+
+  attribute :attendance_status do |record|
+    Status::STATUS_NAMES[record.attendance_status].downcase
+  end
 
   belongs_to :creator
-
-  attribute :student_id do |object|
-    object.statusable_id.to_s if object.statusable_type == 'User'
-  end
-
-  attribute :enrollment_id do |object|
-    object.statusable_id.to_s if object.statusable_type == 'Enrollment'
-  end
+  belongs_to :statusable, polymorphic: true
 end
