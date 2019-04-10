171d268f98a718a9bebe398d9b60429a277d61e5
Eric Artzt <eric.artzt@avalara.com>
2019-04-10 06:43:18 -0700

    A spike on CSS likely to be reverted

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,22 +1,29 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const postcssImport = require('postcss-import');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssNested = require('postcss-nested');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {});
+  const app = new EmberApp(defaults, {
+    postcssOptions: {
+      compile: {
+        plugins: [
+          { module: postcssImport },
+          { module: postcssPresetEnv },
+          { module: postcssNested },
+        ],
+      },
+    },
+    nodeAssets: {
+      pikaday: {
+        vendor: ['css/pikaday.css'],
+      },
+    },
+  });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  app.import('vendor/pikaday/css/pikaday.css');
 
   return app.toTree();
 };
