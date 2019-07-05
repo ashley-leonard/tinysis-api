c29f0b1dff49c15df34b6ca79eb75d8efd6dff32
Eric Artzt <artzte@baddabigboom.com>
2019-07-04 23:06:22 -0700

    titling

--- app/controllers/settings-contract-category.js
+++ app/controllers/settings-contract-category.js
@@ -1,28 +1,33 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 import { summarizeValidationError } from '../utils/response-utils';
 
 export default Controller.extend({
   flashMessages: service(),
   tinyData: service(),
 
-  actions: {
-    togglePublic(event) {
+  categoryName: computed('model', function () {
+    return this.model.attributes.name || 'Untitled';
+  }),
 
-    },
+  actions: {
     async save(data) {
       try {
-        return await this.tinyData.fetch(`/api/admin/contract-categories/${data.id}`, {
+        await this.tinyData.fetch(`/api/admin/contract-categories/${data.id}`, {
           method: 'PUT',
           body: JSON.stringify({ data }),
         });
+
+        this.flashMessages.success(`Category ${data.attributes.name} was updated`);
+        this.transitionToRoute('settings-contract-categories.index');
       } catch (e) {
         this.flashMessages.alert(summarizeValidationError(e));
         return e;
       }
     },
     reportError(error) {
-      this.flashMessages.error(error);
+      this.flashMessages.alert(error);
     },
   },
 });
