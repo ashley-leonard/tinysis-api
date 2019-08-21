b8f6c59650a8121fad8783709c11cb6f77e4dccf
Eric Artzt <artzte@baddabigboom.com>
2019-08-21 06:15:45 -0700

    Student route

---
+++ app/routes/student.js
@@ -0,0 +1,18 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default Route.extend({
+  tinyData: service(),
+  model(params) {
+    return this.tinyData.fetch(`/api/students/${params.id}`);
+  },
+  afterModel(model, transition) {
+    transition.data.student = model.data;
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.setProperties({
+      student: model.data,
+    });
+  },
+});
