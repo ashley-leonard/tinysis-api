7acfadbf9140f7ee430c5931bdfefc2a4faa74c9
Eric Artzt <eric.artzt@avalara.com>
2019-09-10 07:40:21 -0400

    template lint - disable no-invalid-interactive

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,5 +4,9 @@ module.exports = {
   extends: 'recommended',
   rules: {
     'table-groups': false,
+
+    // this seems to preclude drag/drop events on dom elements
+    // which does not make sense to me. it's also a pain for A without an href.
+    'no-invalid-interactive': false,
   },
 };
