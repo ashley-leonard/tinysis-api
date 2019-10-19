0b6d309a635652f31d77747422c8c8b5545d5fca
Eric Artzt <artzte@baddabigboom.com>
2019-10-19 16:59:40 -0700

    to ember 3.12

--- app/controllers/student-credits.js
+++ app/controllers/student-credits.js
@@ -1,7 +1,6 @@
 import Controller from '@ember/controller';
 import dayjs from 'dayjs';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 import { resolve } from 'rsvp';
 import { replaceModel } from '../utils/json-api';
 
