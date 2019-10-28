df1547498911968a1a5f4bca5e4c877513a49032
Eric Artzt <artzte@baddabigboom.com>
2019-10-27 18:21:23 -0700

    credits worksheet tests

--- tests/integration/components/credits-worksheet-test.js
+++ tests/integration/components/credits-worksheet-test.js
@@ -1,26 +1,69 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import {
+  render, find, click, findAll,
+} from '@ember/test-helpers';
+import { resolve } from 'rsvp';
 import hbs from 'htmlbars-inline-precompile';
+import studentCreditAssignmentsFixture from '../../fixtures/student-credit-assignments-with-combined';
+import { stubTinyData } from '../../helpers/stub-tiny-data';
+
+let tinyData;
+let creditAssignments;
+let requests;
 
 module('Integration | Component | credits-worksheet', (hooks) => {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    tinyData = stubTinyData();
+    tinyData.addResult(studentCreditAssignmentsFixture);
 
-    await render(hbs`<CreditsWorksheet />`);
+    creditAssignments = tinyData.get('creditAssignment');
 
-    assert.equal(this.element.textContent.trim(), '');
+    requests = [];
 
-    // Template block usage:
+    this.setProperties({
+      creditAssignments,
+      splitCredits: (model) => {
+        requests.push({ type: 'split', model });
+        return resolve(model);
+      },
+      combineCredits: (model) => {
+        requests.push({ type: 'split', model });
+      },
+      approveCredit: (model) => {
+        requests.push({ type: 'approve', model });
+      },
+    });
+  });
+
+  test('it renders with a set of student credits', async (assert) => {
     await render(hbs`
-      <CreditsWorksheet>
-        template block text
-      </CreditsWorksheet>
+      <CreditsWorksheet
+        @student={{student}}
+        @creditAssignments={{creditAssignments}}
+        @showCombine={{this.combineCredits}}
+        @splitCredit={{this.splitCredit}}
+        @approveCredit={{this.approveCredit}}
+      />    
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.ok(find('[data-test-credits-worksheet]'), 'worksheet table rendered');
+
+    const unbatched = creditAssignments.filter(ca => !ca.relationships.creditTransmittalBatch.data);
+    const countParagraph = find(`[data-test-count-paragraph="${unbatched.length}"]`);
+    assert.ok(countParagraph, 'the expected count was rendered');
+
+    assert.equal(findAll('tbody tr').length, unbatched.length, 'expected count of rows rendered');
+
+    const creditWithChildren = unbatched.find(ca => ca.relationships.childCreditAssignments.data.length);
+    const unapprovedCredit = unbatched.find(ca => ca.attributes.districtFinalizeApprovedOn);
+    const approvedCredit = unbatched.find(ca => !ca.attributes.districtFinalizeApprovedOn);
+    const combinableCredits = unbatched
+      .filter(ca => !ca.relationships.childCreditAssignments.data.length)
+      .slice(0, 2);
+
+    debugger;
   });
 });
